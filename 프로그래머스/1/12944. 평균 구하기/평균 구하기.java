631f50865bd5fd13c1fdd640501ee7cbfc00bade
seaStamp <seastamp10@gmail.com>
2023-11-09 15:15:49 +0900

    [level 1] Title: 평균 구하기, Time: 2.00 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,10 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        for (int n : arr){
-            answer += n;
-        }
-        answer /= arr.length;
-        return answer;
+        return Arrays.stream(arr).average().getAsDouble();
     }
 }
