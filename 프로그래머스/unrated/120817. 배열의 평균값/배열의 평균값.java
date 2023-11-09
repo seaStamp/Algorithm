9a3ddbece7250e5c5b2ac4560d21f72a81edfb60
seaStamp <seastamp10@gmail.com>
2023-11-09 16:29:32 +0900

    [level unrated] Title: 배열의 평균값, Time: 2.66 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public double solution(int[] numbers) {
-        return Arrays.stream(numbers).average().getAsDouble();
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }
