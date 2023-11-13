395177c6bd6761805e992265d93817f89846dd81
seaStamp <seastamp10@gmail.com>
2023-11-13 10:38:39 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,11 +1,8 @@
 class Solution {
     public long solution(long n) {
-        long answer = -1;
-        for(long i = 1; i*i <= n; i++){
-            if (n == i*i){
-                answer = (i+1)*(i+1);
-            }
+        if((long)Math.pow((int)Math.sqrt(n),2) == n){
+            return (long)Math.pow((int)Math.sqrt(n) + 1, 2);
         }
-        return answer;
+        return -1;
     }
 }
