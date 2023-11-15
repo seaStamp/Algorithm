f0000ae9c703fbf4a9e9b9e27833242d6132cfe5
seaStamp <seastamp10@gmail.com>
2023-11-15 09:49:30 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 2.94 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = Arrays.stream(arr).filter(a -> a % divisor == 0).toArray();
+        if(answer.length == 0){
+            answer = new int[] {-1};
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
