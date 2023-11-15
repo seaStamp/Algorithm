fcaabc6cbde5f4a37db2f8a0f41981f5dc7e027a
seaStamp <seastamp10@gmail.com>
2023-11-15 09:24:56 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 13.88 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,10 +1,9 @@
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String[] seoul) {
         String answer = "김서방은 ";
-        answer += Arrays.stream(seoul).collect(Collectors.toList()).indexOf("Kim")+"에 있다";
+        answer += Arrays.asList(seoul).indexOf("Kim")+"에 있다";
         return answer;
     }
 }
