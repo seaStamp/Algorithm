2656460238725d1b4d45d3bd2f9f054108603b80
seaStamp <seastamp10@gmail.com>
2023-11-16 09:18:31 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.43 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int min = Arrays.stream(arr).min().getAsInt();
+        int[] answer = Arrays.stream(arr).filter(i -> i != min).toArray();
+        if (answer.length == 0){
+            return new int[]{-1};
+        }
+        return answer;
+    }
+}
