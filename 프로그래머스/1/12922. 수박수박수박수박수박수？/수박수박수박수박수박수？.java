15ceec0f1ce23e55022eb863c631f14e50161ecf
seaStamp <seastamp10@gmail.com>
2023-11-17 09:22:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.06 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append((i % 2 == 0) ? "수":"박");
+        }
+        return sb.toString();
+    }
+}
