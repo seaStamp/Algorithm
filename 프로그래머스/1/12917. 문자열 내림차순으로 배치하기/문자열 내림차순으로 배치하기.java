334faa593f41f18ad5a619bf37c51e4d8474eecc
seaStamp <seastamp10@gmail.com>
2023-11-20 09:32:38 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.39 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        char [] answer = s.toCharArray();
+        Arrays.sort(answer);
+        return new StringBuilder(new String(answer)).reverse().toString();
+    }
+}
