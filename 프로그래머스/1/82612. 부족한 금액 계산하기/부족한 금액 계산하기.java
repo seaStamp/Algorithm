59615ecd1badc70c6b3aee66e880893e372bc536
seaStamp <seastamp10@gmail.com>
2023-11-20 09:41:24 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -1,6 +1,5 @@
 class Solution {
     public long solution(int price, int money, int count) {
-        long answer = (long)(count * (count + 1) / 2) * price - money;
-        return (answer <= 0)? 0 :answer;
+        return Math.max((long)(count * (count + 1) / 2) * price - money, 0);
     }
 }
