08f6783251afb3fa931743ba46104fef51fb0d8f
seaStamp <seastamp10@gmail.com>
2023-11-20 09:23:23 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -2,20 +2,9 @@ class Solution {
     public int solution(int left, int right) {
         int answer = 0;
         while(left <= right) {
-            answer += (countDivisor(left) % 2 == 0)?left:-left;
+            answer += (left % Math.sqrt(left) == 0)? -left : left;
             left++;
         }
         return answer;
-    }
-    
-    public int countDivisor(int num){
-        int count = 0;
-        int sqrt = (int) Math.sqrt(num);
-        for(int i = 1; i <= sqrt ; i++){
-            if(num % i == 0){
-                count += (num / i == i) ? 1 : 2;
-            }
-        }
-        return count;
     }
 }
