65b3f7eb58bb71e5ce022fef66fe9f040ce45b89
seaStamp <seastamp10@gmail.com>
2023-11-23 09:42:59 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,14 +1,16 @@
-
 class Solution {
-  public String solution(String s) {
-
-      StringBuilder answer = new StringBuilder();
-      String[] array = s.split("");  
-      int cnt = 0;
-      for(String str : array) {
-          cnt = str.contains(" ") ? 0 : cnt + 1;
-          answer.append((cnt % 2 == 0) ? str.toLowerCase() : str.toUpperCase()); 
-      }
-      return answer.toString();
-  }
+    public String solution(String s) {
+        char[] answer = s.toCharArray();
+        int cnt = 0;
+        
+        for (int i = 0; i < answer.length; i++) {
+            if(answer[i] == ' '){
+                cnt = 0;
+            } else{
+            answer[i] = (cnt++ % 2 == 0)? Character.toUpperCase(answer[i]) : Character.toLowerCase(answer[i]);
+            }
+        }
+        
+        return String.valueOf(answer);
+    }
 }
