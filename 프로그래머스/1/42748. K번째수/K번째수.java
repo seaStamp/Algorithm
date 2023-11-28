2d09ec1824d2e44e32def295c6ce9eaed604065f
seaStamp <seastamp10@gmail.com>
2023-11-28 09:45:17 +0900

    [level 1] Title: K번째수, Time: 0.43 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i = 0; i < answer.length; i++){
+            int[] subArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(subArray);
+            answer[i] = subArray[commands[i][2] - 1];
+        }
+        
+        return answer;
+    }
+}
