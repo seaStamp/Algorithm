946e3085be6d50c5c81cb52204f05835cb839ae1
seaStamp <seastamp10@gmail.com>
2023-11-28 09:29:37 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.71 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,12 +1,16 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings,(o1, o2)  -> {
-            if(o1.charAt(n) == o2.charAt(n)){
-                return o1.compareTo(o2);
+        Arrays.sort(strings, new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2){
+                if(o1.charAt(n) == o2.charAt(n)){
+                    return o1.compareTo(o2);
+                }
+                return o1.charAt(n) - o2.charAt(n);
             }
-            return o1.charAt(n) - o2.charAt(n);
         });
         return strings;
     }
